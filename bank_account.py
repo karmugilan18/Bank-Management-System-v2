023416cd849f28bcd9f926987a76dda4ab83efcb
karmugilan18 <elangovankarmugilan@gmail.com>
2026-08-01 17:27:38 +0530

    Lesson 2: Added deposit, withdraw, check_balance methods with validation

--- bank_account.py
+++ bank_account.py
@@ -13,9 +13,48 @@ class BankAccount:
         print(f"Balance        : {self.balance:.2f}")
         print("----------------------------------")
 
+    def deposit (self, amount):
+        if amount<=0:
+            print (" Deposit amount must be greater than zero ")
+            return 
+        self.balance += amount 
+        print(f"₹{amount:.2f} deposited successfully")
+        print(f" New Balance : ₹{self.balance:.2f} ")
+
+    def withdraw (self, amount):
+        if amount<=0:
+            print (" Withdraw amount must be greater than zero ")
+            return 
+        if amount> self.balance:
+            print("Insufficient Funds")
+            print (f" Available Balance  ₹{self.balance:.2f}")
+            return 
+        self.balance -= amount
+
+        print(f"₹{amount:.2f} withdrawn succesfully.")
+        print (f"  New Balance  ₹{self.balance:.2f}")
+        
+    def check_balance(self):
+        print(f"Current Balance for {self.owner_name} :₹{self.balance:.2f} ")
+
+#--- Testing all Methods ---
 account1 = BankAccount ("ACC01", "Karmugilan" , 5000.0)
-account2 = BankAccount("ACC002" , "Priya" , 12000.0)
+#account2 = BankAccount("ACC002" , "Priya" , 12000.0)
 
 account1.display_info()
-account2.display_info()
+ #account2.display_info()
+
+print("\n === Deposit Tests ====")
+account1.deposit(2000)
+account1.deposit(-500)
+account1.deposit(0)
+
+print("\n === Withdraw tests ===")
+account1.withdraw(1000)
+account1.withdraw(9999)
+account1.withdraw(-200)
+
+print("\n ==== Balance Tests ===")
+account1.check_balance()
+
 
